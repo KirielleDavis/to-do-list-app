bc5c8371e49af0da93a1016dc409f9818dd4ace1
Kirielle Davis <160552612+KirielleDavis@users.noreply.github.com>
2024-08-20 13:55:24 -0700

    Changed Javascript to Jquery

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function newItem () {
+
+//1. Adding a new item to the list:
+
+let li = $('<li></li>');
+let inputValue = $('#input').val();
+let text = inputvalue;
+li.append(text);
+
+if (inputValue === '') {
+    alert("You must write something!");
+} else {
+    let list =$('#list');
+    list.append(li);
+    $('#list').val(''); // delete previous input text
+}
+
+//2. Crossing an item out:
+
+function crossOut() {
+    li.classList.toggle("strike");
+}
+
+li.on("dblclick",crossOut);
+
+//3(i). Adding a delete button
+
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('X'));
+li.append(crossOutButton);
+
+crossOutButton.on("click", deleteListItem);
+
+//3(ii) Adding CLASS DELETE (Display:none) from the CSS:
+function deleteListItem(){
+      li.addClass("delete")
+  }
+
+//4. Reordering the items
+ $('#list').sortable();
+
+}
