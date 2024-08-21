d9ba4a46a9adcb9bbb3d4d727fdfd6cd7a8b5dab
Kirielle Davis <160552612+KirielleDavis@users.noreply.github.com>
2024-08-21 10:23:58 -0700

    Updated format

--- js/script.js
+++ js/script.js
@@ -1,42 +1,42 @@
-function newItem () {
+function newItem() {
 
-//1. Adding a new item to the list:
+    //1. Adding a new item to the list:
 
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-let text = inputValue;
-li.append(text);
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    let text = inputValue;
+    li.append(text);
 
-if (inputValue === '') {
-    alert("You must write something!");
-} else {
-    let list =$('#list');
-    list.append(li);
-    $('#list').val(''); // delete previous input text
-}
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        let list =$('#list');
+        list.append(li);
+        $('#list').val(''); // delete previous input text
+    }
 
-//2. Crossing an item out:
+    //2. Crossing an item out:
 
-function crossOut() {
-    li.classList.toggle("strike");
-}
+    function crossOut() {
+        li.classList.toggle("strike");
+    }
 
-li.on("dblclick",crossOut);
+    li.on("dblclick",crossOut);
 
-//3(i). Adding a delete button
+    //3(i). Adding a delete button
 
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('X'));
-li.append(crossOutButton);
+    let crossOutButton = $('<crossOutButton></crossOutButton>');
+    crossOutButton.append(document.createTextNode('X'));
+    li.append(crossOutButton);
 
-crossOutButton.on("click", deleteListItem);
+    crossOutButton.on("click", deleteListItem);
 
-//3(ii) Adding CLASS DELETE (Display:none) from the CSS:
-function deleteListItem(){
-      li.addClass("delete")
-  }
+    //3(ii) Adding CLASS DELETE (Display:none) from the CSS:
+    function deleteListItem(){
+        li.addClass("delete")
+    }
 
-//4. Reordering the items
- $('#list').sortable();
+    //4. Reordering the items
+    $('#list').sortable();
 
 }
